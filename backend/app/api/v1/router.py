3516d16e8d71aae6a2ab564f0514d2b5e42e4a4d
andelson teufack <teufackandelson123@gmail.com>
2026-05-07 18:20:34 +0100

    ajout du menu conges et permissions

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,7 +1,7 @@
 """Agrège tous les endpoints de l'API v1."""
 
 from fastapi import APIRouter
-from app.api.v1.endpoints import auth, users
+from app.api.v1.endpoints import auth, users, leaves
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
